b18da7aa159924d947be41fb28dbf2082d7a2bc8
Alex Defreitas <alexandreadef@gmail.com>
2016-06-17 11:01:49 -0400

    installed react

---
+++ app/components/App/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Hello!</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ app/main.js
@@ -0,0 +1,9 @@
+import 'babel-polyfill';
+import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+var css = require("./scss/main.scss");
+
+
+ReactDOM.render(<App/>, document.getElementById('app'));
