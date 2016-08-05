d4ff76879025bf9661bb4ca6c0ce1ed7c22c9d1e
Alex Defreitas <alexandreadef@gmail.com>
2016-08-05 10:37:52 -0400

    Adds Reducer

---
+++ app/actions/index.js
@@ -0,0 +1,29 @@
+import { createAction } from 'redux-actions';
+
+import {
+  apiGetIngredients,
+  apiAddIngredient,
+  apiEditIngredient,
+  apiDeleteIngredient,
+} from '../utils/cuisineApi';
+
+import {
+  GET_INGREDIENTS,
+  ADD_INGREDIENT,
+  EDIT_INGREDIENT,
+  DELETE_INGREDIENT,
+  SET_SECTION,
+  SET_VIEW,
+} from '../constants';
+
+export const getIngredients = createAction(GET_INGREDIENTS, apiGetIngredients);
+
+export const addIngredient = createAction(ADD_INGREDIENT, apiAddIngredient);
+
+export const setSection = createAction(SET_SECTION);
+
+export const setView = createAction(SET_VIEW);
+
+export const editIngredient = createAction(EDIT_INGREDIENT, apiEditIngredient);
+
+export const deleteIngredient = createAction(DELETE_INGREDIENT, apiDeleteIngredient);
